1c4f7b981913e0c81245033c6255ebe8522a74a6
German Slautin <german.slautin2@dxc.com>
2024-02-24 11:49:32 +0100

    fixed appeared eslint problems

--- farm-service/.eslintrc.cjs
+++ farm-service/.eslintrc.cjs
@@ -30,5 +30,14 @@ module.exports = {
     "rules": {
         'typescript-eslint/explicit-function-return-type': 'off',
         'react/react-in-jsx-scope': 'off',
+        "indent": ["warn", 2], // 2 spaces indentation
+        "keyword-spacing": ["warn", { "before": true, "after": true }], // Spaces around keywords
+        "space-before-blocks": ["warn", "always"], // Space before block opening braces
+        "space-before-function-paren": ["warn", "always"], // Space before function parentheses
+        "max-len": ["warn", { "code": 80 }], // Maximum line length of 80 characters
+        "linebreak-style": ["warn", "unix"], // Unix line endings
+        "comma-dangle": ["warn", "always-multiline"], // Trailing commas in multiline object and array literals
+        "quotes": ["warn", "single"], // Single quotes for string literals
+        "semi": ["warn", "always"] // Semicolons at the end of statements
     }
 }

a504c7ffcc908fadd4a87b875656b2b137a5447e
gSlautin <german.slautin@rokolabs.com>
2024-04-06 15:40:45 +0200

    created header

--- farm-service/src/app/layouts/baseLayout.tsx
+++ farm-service/src/app/layouts/baseLayout.tsx
@@ -1,12 +1,22 @@
+import Button from "@mui/material/Button";
 import { Layout } from "@/shared/ui";
+import classes from "./baseLayout.module.css";
 
 const TestBottomSlot = () => {
   return <div>test</div>;
 };
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className={classes.header}>
+      <div className={classes.logo}>LOGO</div>
+      <Button color={"error"} variant="contained">
+        Logout
+      </Button>
+    </div>
+  );
 };
+
 export const baseLayout = (
   <Layout bottomSlot={<TestBottomSlot />} headerSlot={<Header />} />
 );
